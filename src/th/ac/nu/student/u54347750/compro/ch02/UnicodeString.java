08b709912b41e37b03007bdd622a776ef21aa729
Administrator <my_fang55@hotmail.com>
2012-07-11 22:11:04 +0700

    UnicodeString

---
+++ src/th/ac/nu/student/u54347750/compro/ch02/UnicodeString.java
@@ -0,0 +1,10 @@
+package th.ac.nu.student.u54347750.compro.ch02;
+
+public class UnicodeString {
+
+    public static void main(String[] args) {
+        String ข้อความ = "สวัสดี";
+            System.out.println(ข้อความ);
+
+    }
+}
