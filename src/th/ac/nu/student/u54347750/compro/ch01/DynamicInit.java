faca3f4fb7851396aa54c7d94f7fed3ee6d561f0
Administrator <my_fang55@hotmail.com>
2012-07-11 21:49:41 +0700

    DynamicInit

---
+++ src/th/ac/nu/student/u54347750/compro/ch01/DynamicInit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54347750.compro.ch01;
+
+/**
+ *
+ * @author Administrator
+ */
+public class DynamicInit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        System.out.println(num1+num2);
+    }
+}
