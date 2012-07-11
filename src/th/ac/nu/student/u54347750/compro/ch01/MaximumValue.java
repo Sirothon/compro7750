b338404a8195282fc41bd170c2c59264ebb639ee
Administrator <my_fang55@hotmail.com>
2012-07-11 21:51:13 +0700

    MaximumValue

---
+++ src/th/ac/nu/student/u54347750/compro/ch01/MaximumValue.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54347750.compro.ch01;
+
+/**
+ *
+ * @author Administrator
+ */
+public class MaximumValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num = 2147483647;
+        System.out.println(num); //2147483647
+        System.out.println(num+1); //-214783648
+        int num2 = -214783648;
+        System.out.println(num2);
+        System.out.println(num2-1);
+        
+        
+        
+    }
+}
